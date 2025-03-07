a30754f8e8a13d42ad51d7ddfadb991586910504
ldphat99 <ldphat99@gmail.com>
2025-03-07 13:31:52 +0700

    chore: remove google_fonts package

--- flutter_app/lib/screens/result_screen.dart
+++ flutter_app/lib/screens/result_screen.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_app/models/waste_classification.dart';
-import 'package:google_fonts/google_fonts.dart';
 
 class ResultScreen extends StatelessWidget {
   final WasteClassification result;
@@ -20,7 +19,7 @@ class ResultScreen extends StatelessWidget {
 					Text('Waste Type: ${result.className}', style: TextStyle(fontSize: 18),),
 					Text('Confidence: ${(result.confidence * 100).toStringAsFixed(3)}%', style: TextStyle(fontSize: 18),),
 					Text('Category: ${result.category}', style: TextStyle(fontSize: 18),),
-					Text('Disposal Instructions: ${result.disposalIntruction}', style: GoogleFonts.notoSans(fontSize: 18),),
+					Text('Disposal Instructions: ${result.disposalIntruction}', style: TextStyle(fontSize: 18),),
 					SizedBox(height: 20,),
 					Text('Top predictions:', style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
 					...result.topPredictions.map((pred) => Text(
