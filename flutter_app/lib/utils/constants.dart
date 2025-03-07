b06efbb45ab560c0c9834abb932bd8bb1e2d124a
ldphat99 <ldphat99@gmail.com>
2025-03-07 13:27:02 +0700

    feat: setup app constants

---
+++ flutter_app/lib/utils/constants.dart
@@ -0,0 +1,5 @@
+class Constants {
+    // NOTE: If you use Android (Virtual) and connect with your local computer: 10.0.2.2
+    //  Don't use http://localhost
+	static const String apiUrl = "http://10.0.2.2:8000";
+}
