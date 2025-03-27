386c2206b26a507830512cdd238a5d69133e21e3
ldphat99 <ldphat99@gmail.com>
2025-03-27 21:46:06 +0700

    chore: add url for real device

--- flutter_app/lib/utils/constants.dart
+++ flutter_app/lib/utils/constants.dart
@@ -1,5 +1,9 @@
 class Constants {
     // NOTE: If you use Android (Virtual) and connect with your local computer: 10.0.2.2
     //  Don't use http://localhost
-	static const String apiUrl = "http://10.0.2.2:8000";
+	// static const String apiUrl = "http://10.0.2.2:8000";
+
+	// Real Device: Use your local IP Address
+	// OPEN cmd => ipconfig => Wireless LAN adapter Wi-Fi: IPv4 Address
+	static const String apiUrl = "http://192.168.1.45:8000";
 }
